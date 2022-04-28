f977c233c63f023a287f115af3feed78a19b71be
janginSchool <104504623+janginSchool@users.noreply.github.com>
2022-04-28 13:37:03 +0200

    Add files via upload

---
+++ action.php
@@ -0,0 +1,146 @@
+<?php
+//index.php
+
+$salutation = '';
+$firstname = '';
+$lastname = '';
+$street = '';
+$plz = '';
+$phonenumber = '';
+$birthday = '';
+$email = '';
+$userPassword = '';
+$userPasswordConfirmed = '';
+$city = '';
+$state = '';
+$userId = random_int(1000000, 9999999);
+
+if (isset($_POST["submit"])) {
+
+  if (!empty($_POST["salutation"])) {
+    $salutation = clean_text($_POST["salutation"]);
+  }
+
+  if (!empty($_POST["firstname"])) {
+    $firstname = clean_text($_POST["firstname"]);
+  }
+
+  if (!empty($_POST["lastname"])) {
+    $lastname = clean_text($_POST["lastname"]);
+  }
+
+  if (!empty($_POST["street"])) {
+    $street = clean_text($_POST["street"]);
+  }
+
+  if (!empty($_POST["plz"])) {
+    $plz = clean_text($_POST["plz"]);
+  }
+
+  if (!empty($_POST["phonenumber"])) {
+    $phonenumber = clean_text($_POST["phonenumber"]);
+  }
+
+  if (!empty($_POST["birthday"])) {
+    $birthday = clean_text($_POST["birthday"]);
+  }
+
+  if (!empty($_POST["email"])) {
+    $email = clean_text($_POST["email"]);
+  }
+
+  if (!empty($_POST["password"])) {
+    $userPassword = clean_text($_POST["password"]);
+    $userPasswordConfirmed = password_hash($userPassword, PASSWORD_DEFAULT);
+  }
+
+  if (!empty($_POST["city"])) {
+    $city = clean_text($_POST["city"]);
+  }
+
+  if (!empty($_POST["state"])) {
+    $state = clean_text($_POST["state"]);
+  }
+
+  $file_open = fopen("users.csv", "a");
+  $no_rows = count(file("users.csv"));
+
+  if ($no_rows > 1) {
+    $no_rows = ($no_rows - 1) + 1;
+  }
+
+  $form_data = array(
+    'sr_no'  => $no_rows,
+    'salutation'  => $salutation,
+    'firstname'  => $firstname,
+    'lastname' => $lastname,
+    'street' => $street,
+    'plz' => $plz,
+    'phonenumber' => $phonenumber,
+    'birthday' => $birthday,
+    'email' => $email,
+    'password' => $userPasswordConfirmed,
+    'city' => $city,
+    'state' => $state
+  );
+
+  fputcsv($file_open, $form_data);
+
+  fclose($file_open);
+
+  $file_name = $_FILES['file']['name'];
+  $file_type = $_FILES['file']['type'];
+  $file_size = $_FILES['file']['size'];
+  $file_tem_loc = $_FILES['file']['tmp_name'];
+  $file_store = '/var/www/html/userFiles/' . $file_name;
+
+  if (move_uploaded_file($file_tem_loc, $file_store)) {
+    echo "File uploaded successfuly";
+    echo "<br>";
+  }
+
+  echo "Thanks for registration <br>";
+  echo "<a href='main.html'>Back to main website</a>";
+
+  try {
+
+    $servername = 'localhost';
+    $username = 'jangin';
+    $password = 'Jangin1234!';
+    $db = 'test';
+
+    $connection = new PDO("mysql:host={$servername};dbname={$db}", $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "insert into users values('$userId', '$firstname', '$lastname', '$salutation', '$email', '$birthday', '$userPasswordConfirmed', '$phonenumber', '$street', '$plz', '$city', '$state')";
+
+    $connection->exec($sql);
+  } catch (Exception $e) {
+    echo ('Fehler: ' . $e->getMessage());
+    echo phpinfo();
+    exit();
+  }
+
+  $connection = null;
+
+  $salutation = '';
+  $firstname = '';
+  $lastname = '';
+  $street = '';
+  $plz = '';
+  $phonenumber = '';
+  $birthday = '';
+  $email = '';
+  $userPassword = '';
+  $userPasswordConfirmed = '';
+  $city = '';
+  $state = '';
+}
+
+function clean_text($string)
+{
+  $string = trim($string);
+  $string = stripslashes($string);
+  $string = htmlspecialchars($string);
+  return $string;
+}
---
+++ action_page.php
@@ -0,0 +1,83 @@
+<?php 
+
+session_start(); 
+
+if (isset($_POST['email']) && isset($_POST['psw'])) {
+
+    function validate($data){
+
+       $data = trim($data);
+
+       $data = stripslashes($data);
+
+       $data = htmlspecialchars($data);
+
+       return $data;
+
+    }
+
+    $uname = validate($_POST['email']);
+
+    $pass = validate($_POST['psw']);
+
+    if (empty($email)) {
+
+        header("Location: index.php?error=Email is required");
+
+        exit();
+
+    }else if(empty($psw)){
+
+        header("Location: index.php?error=Password is required");
+
+        exit();
+
+    }else{
+
+        $sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) === 1) {
+
+            $row = mysqli_fetch_assoc($result);
+
+            if ($row['user_name'] === $uname && $row['password'] === $pass) {
+
+                echo "Logged in!";
+
+                $_SESSION['user_name'] = $row['user_name'];
+
+                $_SESSION['name'] = $row['name'];
+
+                $_SESSION['id'] = $row['id'];
+
+                header("Location: home.php");
+
+                exit();
+
+            }else{
+
+                header("Location: index.php?error=Incorect User name or password");
+
+                exit();
+
+            }
+
+        }else{
+
+            header("Location: index.php?error=Incorect User name or password");
+
+            exit();
+
+        }
+
+    }
+
+}else{
+
+    header("Location: index.php");
+
+    exit();
+
+}
